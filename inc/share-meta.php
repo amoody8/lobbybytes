8fddf1e333f59f03a765cb63687e6c03bbb7d056
amoody8 <allan.t.moody@gmail.com>
2014-03-28 17:02:03 -0400

    Added Sharing Functionality
    + Used the addThis API to add sharing functionality.
    + Created custom share buttons.

--- inc/share-meta.php
+++ inc/share-meta.php
@@ -1,12 +1,16 @@
 <aside class="meta">
+<div class="addthis_toolbox addthis_default_style">
 	<div class="share">
 		<strong>Share</strong>
 		<ul class="social-links">
-			<li class="facebook"><a href="<?php get_field('share_facebook'); ?>">Facebook</a></li>
-			<li class="twitter"><a href="<?php get_field('share_twitter'); ?>">Twitter</a></li>
-			<li class="email"><a href="mailto:<?php get_field('share_email'); ?>">Email</a></li>
+			<li class="facebook"><a class="addthis_button_facebook"></a><!--<a href="<?php get_field('share_facebook'); ?>">Facebook</a>--></li>
+			<li class="twitter"><a class="addthis_button_twitter"></a><!--<a href="<?php get_field('share_twitter'); ?>">Twitter</a>--></li>
+			<li class="email"><a class="addthis_button_email"></a><!--<a href="mailto:<?php get_field('share_email'); ?>">Email</a>--></li>
 		</ul>
 	</div>
+	</div>
+	<script type="text/javascript">var addthis_config = {"data_track_addressbar":true,"ui_use_css":false};</script>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5334eeb50950f620"></script>
 	<div class="buttons">
 		<a href="<?php get_field('tickets'); ?>">Tickets</a>
 		<a href="mailto:<?php get_field('rsvp'); ?>">Rsvp</a>
