5ac4ac1f6554687ee0b72d6346322e7cacec664e
amoody8 <allan.t.moody@gmail.com>
2014-04-16 11:09:05 -0400

    Added new filter for scheduled posts
    allows any visitor to see upcoming events (scheduled posts).

--- functions.php
+++ functions.php
@@ -20,7 +20,12 @@ require_once('inc/template-tags.php' );
   		'id' => 'stay-mobile',
   		'description' => __( 'Widgets in this area will be shown on the Home page in the right column' ),
 	));
-	
+//Register Sidebars
+	register_sidebar(array(
+  		'name' => __( 'Map' ),
+  		'id' => 'map',
+  		'description' => __( 'This widget should only contain the google map shortcode' ),
+	));
 //Register Google CDN jQuery
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
@@ -223,6 +228,57 @@ endif;
 //====================//
 //*****  WIDGETS *****//
 //====================//
+
+//About Us widget
+class about_widget extends WP_Widget {
+ 
+ 
+    /** constructor -- name this the same as the class above */
+    function about_widget() {
+        parent::WP_Widget(false, $name = 'About Us Widget');	
+    }
+ 
+    /** @see WP_Widget::widget -- do not rename this */
+    function widget($args, $instance) {	
+        extract( $args );
+        $title 		= apply_filters('widget_title', $instance['title']);
+        $message 	= $instance['message'];
+        ?>
+          <?php echo $before_widget; ?>
+                  		<?php if ( $title )
+                        	echo $before_title . $title . $after_title; ?>
+                        	<div class="textwidget">
+								<?php the_field('about','option'); ?>
+							</div>
+               				<?php echo $after_widget; ?> 
+        <?php
+    }
+ 
+    /** @see WP_Widget::update -- do not rename this */
+    function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['message'] = strip_tags($new_instance['message']);
+        return $instance;
+    }
+ 
+    /** @see WP_Widget::form -- do not rename this */
+    function form($instance) {	
+ 
+        $title 		= esc_attr($instance['title']);
+        $message	= esc_attr($instance['message']);
+        ?>
+         <p>
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+        </p>
+        
+        <?php 
+    }
+ 
+ 
+} // end class about_widget
+add_action('widgets_init', create_function('', 'return register_widget("about_widget");'));
 
 //Shop widget
 class shop_widget extends WP_Widget {
@@ -829,4 +885,20 @@ function eventimg(){
 	<?php }
 	}
 	endif;
+if ( ! function_exists( 'map' ) ) :	
+function the_map(){
+	 dynamic_sidebar( 'map' );
+}
+endif;
+
+function show_future_posts($posts)
+{
+   global $wp_query, $wpdb;
+   if(is_single() && $wp_query->post_count == 0)
+   {
+      $posts = $wpdb->get_results($wp_query->request);
+   }
+   return $posts;
+}
+add_filter('the_posts', 'show_future_posts');
 ?>
